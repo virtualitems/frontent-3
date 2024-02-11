7566d838f8a5e9451c1e6b053d19a733cba41c71
Alejandro CR <contacto@alejandrocr.co>
2024-02-11 17:01:20 -0500

    create PlanCard component

---
+++ src/videos/components/PlanCard.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import css from './PlanCard.module.css';
+
+
+/**
+ * @description User's plan state overview card
+ * @param {Object} props
+ * @returns {JSX.Element}
+ */
+function PlanCard(props) {
+  return (
+    <section className={`${css.card} rounded`}>
+      <div className="row">
+        <div className="col-12">
+          <h1 className={css.title}>
+            Mi Plan - Plus
+          </h1>
+          <span className={css.renewal}>
+            El uso se renueva el: 3-May-23
+          </span>
+        </div>
+        <div className="col-12">
+          <div className={css.currentStorage}>
+            <span className={css.consumptionName}>Almacenamiento</span>
+            <span className={css.consumptionValue}>24.5 GB/1 TB</span>
+          </div>
+          <div className={css.consumptionBar}>
+            <progress value="80" max="100"></progress>
+          </div>
+        </div>
+        <div className="col-12">
+          <div className={css.currentBandwidth}>
+            <span className={css.consumptionName}>Banda Mensual</span>
+            <span className={css.consumptionValue}>3.4 TB/5 TB</span>
+          </div>
+          <div className={css.consumptionBar}>
+            <progress value="70" max="100"></progress>
+          </div>
+        </div>
+        <div className={`col-12 ${css.managePlan}`}>
+          <button className={css.btnPrimary}>Administrar Plan</button>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+
+export default PlanCard;
