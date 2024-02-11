751d0e5418f6f5d4acc2481d5ecb6556ce3214d8
Alejandro CR <contacto@alejandrocr.co>
2024-02-11 18:35:35 -0500

    create SideMenu component

---
+++ src/videos/components/SideMenu.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import css from './SideMenu.module.css';
+import PlanCard from './PlanCard';
+
+
+/**
+ * @description Left side menu
+ * @param {Object} props
+ * @returns {JSX.Element}
+ */
+function SideMenu(props) {
+
+  const propsitems = [
+    {
+      text: 'Dashboard',
+      icon: 'fa-solid fa-bars-progress',
+      active: false,
+    },
+    {
+      text: 'Videos',
+      icon: 'fa-regular fa-folder',
+      active: true,
+    },
+    {
+      text: 'Player',
+      icon: 'fa-regular fa-circle-play',
+      active: false,
+    },
+    {
+      text: 'Analytics',
+      icon: 'fa-solid fa-chart-column',
+      active: false,
+    },
+    {
+      text: 'Configuraciones',
+      icon: 'fa-solid fa-gear',
+      active: false,
+    }
+  ];
+
+  propsitems.push(
+    {
+      text: 'Ayuda',
+      icon: 'fas fa-question-circle',
+      active: false,
+    }
+  );
+
+  const items = propsitems;
+
+  return (
+    <div className={`rounded ${css.sideMenu}`}>
+      <div className={`row ${css.itemsList}`}>
+
+        <div className={`col-12 ${css.headerContainer}`}>
+          <div className={css.header}>
+            <img src="/images/menu-hero.png" alt="menu hero" width="100%" />
+          </div>
+        </div>
+
+        {
+          items.map((item, index) => {
+            return (
+              <div key={index} className={`col-12 ${css.itemContainer}`}>
+                <div className={`${css.item} ${item.active ? css.itemActive : ''}`}>
+                  <div className={css.itemIcon}>
+                    <i className={item.icon}></i>
+                  </div>
+                  <div className={css.itemText}>
+                    <span>{item.text}</span>
+                  </div>
+                </div>
+              </div>
+            )
+          })
+        }
+
+        <div className={`col-12 ${css.planCardContainer}`}>
+          <PlanCard />
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+
+export default SideMenu;
