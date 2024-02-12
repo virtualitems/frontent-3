b16ca6c52434c61ce2161a358190672d1024eb8c
Alejandro CR <contacto@alejandrocr.co>
2024-02-11 20:55:16 -0500

    create videos page

---
+++ src/videos/VideosPage.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import SideMenu from './components/SideMenu';
+import VideosManager from './components/VideosManager';
+
+
+/**
+ * @description Videos web page
+ * @param {Object} props
+ * @returns {JSX.Element}
+ */
+function VideosPage(props) {
+  return (
+    <div className="row p-3">
+      <div className="col-3">
+        <SideMenu />
+      </div>
+      <div className="col-9">
+        <VideosManager />
+      </div>
+    </div>
+  )
+}
+
+
+export default VideosPage;
